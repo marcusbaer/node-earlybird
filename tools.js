0b5c51389ae03f5f269c992013564850cddd149d
Marcus Baer <marcus.baer@greenpocket.de>
2013-06-12 10:21:07 +0200

    Clean up tools.js

--- tools.js
+++ tools.js
@@ -1,15 +1,8 @@
 ﻿var argv = require('optimist').argv;
 var fs = require('fs');
 var sys = require('util');
-var _ = require('underscore');
-var childProcess = require('child_process');
-var data = null;
 var serviceScript = null;
-
-if (argv.data) {
-	data = require(argv.data);
-	var myList = data.myList;
-}
+//var childProcess = require('child_process');
 
 var port = argv.p || '80';
 var serviceUrl = argv.u || 'service';
@@ -142,13 +135,6 @@ var static = function (req, res) {
 exports.static = static;
 exports.service = service;
 
-function loadTemplate (filename, callback) {
-    fs.readFile(filename, 'utf8', function(err, data) {
-        if(err) throw err;
-        callback(data);
-    });
-}
-
 function encode (txt) {
     return new Buffer(txt).toString('base64');
 }
@@ -157,21 +143,28 @@ function decode (txt) {
     return new Buffer(txt, 'base64').toString('utf8');
 }
 
-function ls (options, callback) {
-    var proc = childProcess.exec('ls', function (error, stdout, stderr) {
-        if (error) {
-            console.log(error.stack);
-            console.log('Error code: '+error.code);
-            console.log('Signal received: '+error.signal);
-        }
-//		console.log('Child Process STDOUT: '+stdout);
-//		console.log('Child Process STDERR: '+stderr);
-    });
+//function loadTemplate (filename, callback) {
+//    fs.readFile(filename, 'utf8', function(err, data) {
+//        if(err) throw err;
+//        callback(data);
+//    });
+//}
 
-    proc.on('exit', function (code) {
-//		sys.log('Child process exited with exit code '+code);
-        if (callback) {
-            callback();
-        }
-    });
-}
+//function ls (options, callback) {
+//    var proc = childProcess.exec('ls', function (error, stdout, stderr) {
+//        if (error) {
+//            console.log(error.stack);
+//            console.log('Error code: '+error.code);
+//            console.log('Signal received: '+error.signal);
+//        }
+////		console.log('Child Process STDOUT: '+stdout);
+////		console.log('Child Process STDERR: '+stderr);
+//    });
+//
+//    proc.on('exit', function (code) {
+////		sys.log('Child process exited with exit code '+code);
+//        if (callback) {
+//            callback();
+//        }
+//    });
+//}
